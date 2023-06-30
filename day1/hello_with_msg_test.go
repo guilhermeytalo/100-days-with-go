d957a1f18ee7a18b1ce33c7d1b856ac069edea8e
Guilherme Ytalo <gydaytalo@gmail.com>
2023-06-29 23:07:49 -0300

    fix: error on assertCorrectMsg

--- day1/hello_with_msg_test.go
+++ day1/hello_with_msg_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHelloMsg(t *testing.T) {
-	t.Run("Saying helllo to people", func(t *testing.T) {
+	t.Run("Saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 		assertCorrectMsg(t, got, want)
@@ -14,9 +14,10 @@ func TestHelloMsg(t *testing.T) {
 		want := "Hello, World"
 		assertCorrectMsg(t, got, want)
 	})
+
 }
 
-func assertCorrectMsg(t testing.TB, got, want, string) {
+func assertCorrectMsg(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
